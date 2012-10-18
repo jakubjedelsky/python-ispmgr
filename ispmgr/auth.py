b4349ceaa6c57498d55fe55b03518fc6d753da83
Jakub Jedelsky <jakub.jedelsky@gmail.com>
2012-10-18 16:05:26 +0200

    First simple class for authentization

---
+++ ispmgr/auth.py
@@ -0,0 +1,24 @@
+import urllib, urllib2
+import json
+
+class Auth(object):
+    """Authorize user against ISPManager instance."""
+
+    def __init__(self, url, username, password,):
+        self.url = url
+        self.sessid = self.authorize(username, password)
+
+    def authorize(self, username, password):
+        params = urllib.urlencode({
+            'func': 'auth',
+            'out': 'json',
+            'username': username,
+            'password': password,
+        })
+        data = urllib2.urlopen("%s?%s" % (self.url, params))
+        out = json.load(data)
+
+        if out.has_key('authfail'):
+            raise RuntimeError('Authorization error, check your credentials')
+
+        return out['auth']
